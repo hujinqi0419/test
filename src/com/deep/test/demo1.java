64036ceba5ead9fa9f5322c8d8c60dd20bc474cd
hujinqi0419 <62985098+hujinqi0419@users.noreply.github.com>
2020-04-01 18:57:37 +0900

    Update demo1.java

--- src/com/deep/test/demo1.java
+++ src/com/deep/test/demo1.java
@@ -5,7 +5,7 @@ package com.deep.test;
  * */
 public class demo1 {
     public static void main(String[] args) {
-
+         System.out.println(第一次下载);
     }
 
     public static void main1(String[] args) {
