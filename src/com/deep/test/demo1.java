a1c2c1a6e29f3735f4af6ea360eade1c92e03561
hujinqi <hujinqi0419@gmail.com>
2020-04-01 19:03:37 +0900

    第一次上传

--- src/com/deep/test/demo1.java
+++ src/com/deep/test/demo1.java
@@ -1,15 +1,12 @@
 package com.deep.test;
 
-/*
- *
- * */
 public class demo1 {
     public static void main(String[] args) {
-         System.out.println(第一次下载);
+         System.out.println("第一次下载");
     }
 
     public static void main1(String[] args) {
-        System.out.println(第1次上传);
+        System.out.println("第1次上传");
     }
 
     public static void main2(String[] args) {
