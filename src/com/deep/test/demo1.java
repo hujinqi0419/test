cc6528f30b715d1eefe16e9b377cfb16b5ce57d5
hujinqi <hujinqi0419@gmail.com>
2020-04-01 18:54:26 +0900

    22222222

---
+++ src/com/deep/test/demo1.java
@@ -0,0 +1,22 @@
+package com.deep.test;
+
+/*
+ *
+ * */
+public class demo1 {
+    public static void main(String[] args) {
+
+    }
+
+    public static void main1(String[] args) {
+
+    }
+
+    public static void main2(String[] args) {
+
+    }
+
+    public static void main3(String[] args) {
+
+    }
+}
